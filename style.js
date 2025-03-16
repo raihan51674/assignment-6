ace11acdf623f66b5ba26e49282e9675d756cabf
mdraihan51674 <mdraihan51674@gmail.com>
2025-03-17 01:40:52 +0600

    all update

--- style.js
+++ style.js
@@ -120,6 +120,8 @@ const loadCategories = () => {
 
 };
 
+
+
    document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("no-data-message").classList.remove("hidden");
   document.getElementById("lesson-container").classList.add("hidden");
@@ -132,14 +134,15 @@ const loadCategoryLesson=(level_no)=>{
   fetch(url)
   .then((res)=>res.json())
   .then((dt)=> {
+
+    document.querySelectorAll("#category-container button").forEach(btn => {
+      btn.classList.remove("active");
+  });
    
-
+    const clickedButton = document.getElementById(`btn-${level_no}`);
+    console.log(clickedButton);
     
-
-
-
-
-  
+      clickedButton.classList.add("active");
 
     
     document.getElementById("no-data-message").classList.add("hidden");
@@ -151,6 +154,43 @@ const loadCategoryLesson=(level_no)=>{
   
 }
 
+const loadLessonDetails=(lessonId)=>{
+  const url =`https://openapi.programming-hero.com/api/word/${lessonId}`
+  fetch(url)
+  .then((res)=>res.json())
+  .then((dt)=>DisplayLessonDetails(dt.data)
+  )
+  
+  
+}
+
+const DisplayLessonDetails=(lesson)=>{
+  console.log(lesson);
+  document.getElementById("lesson_details").showModal();
+  const DetailsContainer = document.getElementById("details-container")
+  DetailsContainer.innerHTML=`
+  <div class="card card-dash bg-base-100  box-border ">
+          <div class="flex flex-col gap-5">
+            <h2 class=" font-bold text-[30px]">${lesson.word} (<i class="fa-solid fa-microphone"></i> :${lesson.pronunciation})</h2>
+            <h2 class="text-[22px] font-bold">Meaning</h2>
+            <h3 class="text-[24px] font-medium">${lesson.meaning}</h3>
+            <h2 class="text-[24px] font-bold">Example</h2>
+        
+            <p class="text-[24px] font-normal">${lesson.sentence}</p>
+            <h2 class="text-[24px] font-semibold">সমার্থক শব্দ গুলো</h2>
+            <div class="flex flex-row gap-3 text-center items-center">
+            <p class="text-[22px] font-normal rounded-md bg-[#D7E4EF] px-3 py-2">${lesson.synonyms[0]}</p> <p class="text-[22px] rounded-md bg-[#D7E4EF] font-normal px-3 py-2">${lesson.synonyms[1]}</p> <p class="text-[22px] rounded-md bg-[#D7E4EF] font-normal px-3 py-2">${lesson.synonyms[2]}</p>
+            
+            </div>
+           
+          </div>
+           </div>
+  
+  `
+  
+
+}
+
 const DisplyCategory = (data) => {
   const categoryContainer = document.getElementById("category-container");
   categoryContainer.innerHTML = ""; // Clear previous categories
@@ -160,7 +200,7 @@ const DisplyCategory = (data) => {
     categoryDiv.innerHTML = `
             <button id="btn-${lesson.level_no}" 
                 onClick="loadCategoryLesson(${lesson.level_no})"
-                class="btn px-3 bg-slate-400 text-black hover:btn-primary text-xl hover:text-white font-semibold border-none">
+                class="py-2 rounded-md px-5 border-2 border-indigo-500/100 text-[#422AD5] hover:bg-[#422AD5] text-[26px] hover:text-white font-semibold"><i class="fa-solid fa-book-open"></i>
                 ${lesson.lessonName}
             </button>`
         ;
@@ -211,14 +251,17 @@ const displayLesson=(data)=>{
     lessonCard.innerHTML=`
     
 
-          <div class=" bg-white text-black flex flex-col items-center text-center rounded-md">
-          <div class=" px-3 py-7 flex flex-col gap-5">
-            <h2 class=" font-bold text-[32px]">${lesson.word}</h2>
+          <div class=" bg-white text-black flex flex-col items-center text-center  rounded-md">
+          <div class=" px-3 py-7 flex flex-col gap-5 ">
+          <div class=" ">
+          <h2 class=" font-bold text-[32px]">${lesson.word}</h2>
             <p class="font-medium text-[20px]">Meaning / Pronunciation</p>
             <h2 class=" font-semibold text-[32px] object-cover">"${lesson.meaning}/${lesson.pronunciation}"</h2>
+          </div>
+            
             <div class="pt-8 flex justify-between">
-              <button class="btn px-3 py-3 border-none bg-gray-300"><img class="w-[30px] h-[30px] " src="https://img.icons8.com/?size=48&id=BzHWSXaTEkEv&format=png" alt=""></button>
-              <button class="btn px-3 py-3 border-none bg-gray-300"><img class="w-[30px] h-[30px] " src="https://img.icons8.com/?size=100&id=41563&format=png" alt=""></button>
+              <button onClick="loadLessonDetails(${lesson.id})" class="btn text-2xl px-5 py-7 border-none bg-gray-300"><i class="fa-solid fa-circle-info"></i></button>
+              <button class="btn text-2xl px-5 py-7 border-none bg-gray-300"><i class="fa-solid fa-volume-high"></i></button>
             </div>
           </div>
         </div>
